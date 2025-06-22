e9a801ffe72d439fae2e3a110f4f8f9157d834a3
gustavobianco <gustavopolobianco@gmail.com>
2025-06-21 22:34:00 -0400

    Update Login.jsx

--- contract_generator_frontend/src/components/Login.jsx
+++ contract_generator_frontend/src/components/Login.jsx
@@ -26,7 +26,7 @@ const Login = ({ onLogin, onSwitchToRegister }) => {
     setError('');
 
     try {
-      const response = await fetch('http://localhost:5002/api/login', {
+      const response = await fetch('https://biancomeister.pythonanywhere.com/api/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
